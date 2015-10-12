57645bc4acb1299610ff819a85804e1169ca2200
XuTongle <xutongle@gmail.com>
2015-10-12 22:13:59 +0800

    update

---
+++ protected/Controller/SiteController.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | Leaps Framework [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011-2014 Leaps Team (http://www.tintsoft.com)
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author XuTongle <xutongle@gmail.com>
+// +----------------------------------------------------------------------
+namespace App\Controller;
+
+class SiteController extends  \Leaps\Core\Controller{
+
+	/**
+	 * 首页
+	 */
+	public function indexAction(){
+		echo 999;
+		return 'aaaa';
+	}
+}
