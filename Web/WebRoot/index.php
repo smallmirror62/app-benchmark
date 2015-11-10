213ee3550a21f4abf07a20259c8c79e9febc5e47
XuTongle <xutongle@gmail.com>
2015-11-10 21:56:42 +0800

    UPDATE

--- Web/WebRoot/index.php
+++ Web/WebRoot/index.php
@@ -9,20 +9,14 @@
 // | Author XuTongle <xutongle@gmail.com>
 // +----------------------------------------------------------------------
 // Good morning the world!
-defined('LEAPS_DEBUG') or define('LEAPS_DEBUG', true);
-defined('LEAPS_ENV') or define('LEAPS_ENV', 'test');
+defined ( 'LEAPS_DEBUG' ) or define ( 'LEAPS_DEBUG', true );
+defined ( 'LEAPS_ENV' ) or define ( 'LEAPS_ENV', 'test' );
 
-require(__DIR__ . '/../../vendor/autoload.php');
-require(__DIR__ . '/../../vendor/leaps/framework/Leaps.php');
-require(__DIR__ . '/../../Common/Config/Bootstrap.php');
-require(__DIR__ . '/../config/bootstrap.php');
+require (__DIR__ . '/../../vendor/autoload.php');
+require (__DIR__ . '/../../vendor/leaps/framework/Leaps.php');
+require (__DIR__ . '/../../Common/Config/Bootstrap.php');
+require (__DIR__ . '/../Config/bootstrap.php');
 
-$config = Leaps\Helper\ArrayHelper::merge(
-		require(__DIR__ . '/../../Common/config/main.php'),
-		require(__DIR__ . '/../../Common/config/main-local.php'),
-		require(__DIR__ . '/../Config/main.php'),
-		require(__DIR__ . '/../Config/main-local.php')
-);
-
-$application = new Leaps\Web\Application($config);
-$application->run();
+$config = Leaps\Helper\ArrayHelper::merge ( require (__DIR__ . '/../../Common/config/main.php'), require (__DIR__ . '/../../Common/config/main-local.php'), require (__DIR__ . '/../Config/main.php'), require (__DIR__ . '/../Config/main-local.php') );
+$application = new Leaps\Web\Application ( $config );
+$application->run ();
