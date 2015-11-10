dae983135c8ccbe2f6b0cd14410940de97a87c64
XuTongle <xutongle@gmail.com>
2015-11-10 22:08:54 +0800

    update

---
+++ Common/Config/main-local.php
@@ -0,0 +1,2 @@
+<?php
+return [ 'services' => [ 'db' => [ 'className' => 'yii\db\Connection','dsn' => 'mysql:host=localhost;dbname=codeforge','username' => 'root','password' => '123456','charset' => 'utf8','tablePrefix' => 'yun_' ],'mailer' => [ 'class' => 'yii\swiftmailer\Mailer','viewPath' => '@common/mail','useFileTransport' => true ] ] ];
