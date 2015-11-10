c155a88cffa7975646044cb6acd3fec8c9d67648
XuTongle <xutongle@gmail.com>
2015-11-10 15:14:13 +0800

    update

--- environments/test/frontend/web/index.php
+++ environments/test/frontend/web/index.php
@@ -1,18 +1,28 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
+// +----------------------------------------------------------------------
+// | Leaps Framework [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011-2014 Leaps Team (http://www.tintsoft.com)
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author XuTongle <xutongle@gmail.com>
+// +----------------------------------------------------------------------
+// Good morning the world!
+defined('LEAPS_DEBUG') or define('LEAPS_DEBUG', true);
+defined('LEAPS_ENV') or define('LEAPS_ENV', 'test');
 
 require(__DIR__ . '/../../vendor/autoload.php');
-require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/../../common/config/bootstrap.php');
+require(__DIR__ . '/../../vendor/leaps/framework/Leaps.php');
+require(__DIR__ . '/../../Common/Config/Bootstrap.php');
 require(__DIR__ . '/../config/bootstrap.php');
 
-$config = yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../../common/config/main.php'),
-    require(__DIR__ . '/../../common/config/main-local.php'),
-    require(__DIR__ . '/../config/main.php'),
-    require(__DIR__ . '/../config/main-local.php')
+$config = Leaps\Helper\ArrayHelper::merge(
+		require(__DIR__ . '/../../Common/config/main.php'),
+		require(__DIR__ . '/../../Common/config/main-local.php'),
+		require(__DIR__ . '/../Config/main.php'),
+		require(__DIR__ . '/../Config/main-local.php')
 );
 
-$application = new yii\web\Application($config);
+$application = new Leaps\Web\Application($config);
 $application->run();
